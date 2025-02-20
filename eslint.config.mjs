83953239e9feaddb1747ee9b0df34623a13461e0
hoontae24 <kadr8941@gmail.com>
2025-02-20 22:01:59 +0900

    add more eslint plugins and rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,38 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier/recommended';
+import reExportSort from 'eslint-plugin-re-export-sort';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tsSortKeys from 'eslint-plugin-typescript-sort-keys';
+import unusedImports from 'eslint-plugin-unused-imports';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   prettier,
+  {
+    // plugins are not supported in the flat config
+    plugins: {
+      're-export-sort': reExportSort,
+      'simple-import-sort': simpleImportSort,
+      'typescript-sort-keys': tsSortKeys,
+      'unused-imports': unusedImports,
+    },
+    // overrides custom rules
+    rules: {
+      're-export-sort/exports': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [['^'], ['^@overdesk'], ['^\\.']],
+        },
+      ],
+      'typescript-sort-keys/interface': [
+        'error',
+        'asc',
+        { caseSensitive: false },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
 );
