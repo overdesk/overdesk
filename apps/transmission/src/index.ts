9859c7c986bf020b46291a0f3d69b022d460e6c7
hoontae24 <kadr8941@gmail.com>
2025-03-17 22:44:14 +0900

    independent servers

--- apps/transmission/src/index.ts
+++ apps/transmission/src/index.ts
@@ -1,3 +1,4 @@
+import { Kafka } from 'kafkajs';
 import { WebSocketServer } from 'ws';
 
 const main = async () => {
@@ -7,13 +8,46 @@ const main = async () => {
     path: '/transmission',
     port,
   });
+  const kafka = new Kafka({
+    brokers: ['localhost:9092'],
+  });
+  const producer = kafka.producer();
+  const consumer = kafka.consumer({ groupId: 'chat-receiver' });
 
-  wss.on('connection', (ws) => {
-    ws.on('message', (data) => {
+  await producer.connect();
+  await consumer.connect();
+
+  wss.on('connection', async (ws) => {
+    ws.on('message', async (data) => {
       const { username, body } = JSON.parse(data.toString());
       const message = JSON.stringify({ username, body });
-      wss.clients.forEach((client) => client.send(message));
+
+      await producer.send({
+        topic: 'chat',
+        messages: [{ value: message }],
+      });
     });
+  });
+
+  await consumer.subscribe({ topic: 'chat', fromBeginning: true });
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const parsedMessage = JSON.parse(message?.value?.toString() ?? '{}');
+      console.log('Received message:', {
+        message: {
+          type: typeof message,
+          value: message,
+          valueToString: message?.value?.toString(),
+        },
+        parsedMessage: { type: typeof parsedMessage, value: parsedMessage },
+      });
+
+      wss.clients.forEach((client) => {
+        if (client) {
+          client.send(JSON.stringify(parsedMessage));
+        }
+      });
+    },
   });
 
   console.log(`Server running on :${port}`);
